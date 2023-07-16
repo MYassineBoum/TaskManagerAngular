35e36cb99a82df95c6cd3c958c61ad7c9d94d3c5
Mohammed-Yassine BOUMEHDI <myboumehdi.2002@gmail.com>
2023-07-16 12:56:41 +0100

    Interface to start an empty array for tasks

--- taskmanager/src/app/input-task/input-task.component.ts
+++ taskmanager/src/app/input-task/input-task.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-
+interface newTasks {
+  taskName: string;
+  reminderSet: boolean;
+}
 @Component({
   selector: 'app-input-task',
   templateUrl: './input-task.component.html',
   styleUrls: ['./input-task.component.scss']
 })
+
+
 export class InputTaskComponent implements OnInit {
 
-  myTasks = [{taskName: 'Internship', reminderSet: false}];
+
+  myTasks: newTasks[] = [];
 
   constructor() { };
 
